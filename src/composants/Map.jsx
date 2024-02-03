43a5a269c41d4183bdbaaeb105e0c3065aede85e
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2024-02-03 16:04:25 -0500

    ajout de cercles sur la carte

--- src/composants/Map.jsx
+++ src/composants/Map.jsx
@@ -1,4 +1,6 @@
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Circle, Marker } from "@react-google-maps/api";
+import { useState } from "react";
+import typesLieux from "./TypesLieux.json";
 
 const containerStyle = {
   width: "400px",
@@ -6,8 +8,8 @@ const containerStyle = {
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523,
+  lat: 45.50493439044056,
+  lng: -73.61313521788279,
 };
 
 export default function Map() {
@@ -15,8 +17,33 @@ export default function Map() {
     id: "google-map-script",
     googleMapsApiKey: process.env.MAPS_API_KEY,
   });
+
+  const [map, setMap] = useState();
+
+  const lieu = typesLieux.ecole;
+
+  const optionsCercle = { strokeColor: lieu.contour, strokeOpacity: 0.8, fillOpacity: 0.4, fillColor: lieu.fill };
+
   return isLoaded ? (
-    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}></GoogleMap>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      options={{ disableDefaultUI: true }}
+      center={center}
+      zoom={12}
+      onLoad={(m) => setMap(m)}
+      onUnmount={() => setMap()}
+    >
+      <Circle options={optionsCercle} center={center} radius={1500} map={map} />
+
+      <Marker
+        position={center}
+        map={map}
+        options={{ title: "Poly!" }}
+        icon={{
+          url: lieu.icon,
+        }}
+      />
+    </GoogleMap>
   ) : (
     <h1>Chargement...</h1>
   );
