81b4aae16911569c6fac0472b6423e8e74644628
Jean-Félix Morency <jean-felix.morency@polymtl.ca>
2024-02-03 13:14:33 -0500

    Dépôt initial

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
