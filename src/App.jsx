539dafd5663b7815186be55afa88d5fef277af49
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2024-02-03 16:53:44 -0500

    style general

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import LeftMenu from "./composants/leftMenu/LeftMenu";
 import Map from "./composants/Map";
+import "./style.css"
 
 export default function App() {
   return (
-    <div>
-      <LeftMenu />
+    <div className="h-screen">
       <Map />
+      <LeftMenu />
     </div>
   );
 }
