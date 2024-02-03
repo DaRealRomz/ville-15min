4b62af3f70f4e136115a5991735ec5a8d659c273
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2024-02-03 17:59:03 -0500

    plusieurs emplacements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import LeftMenu from "./composants/leftMenu/LeftMenu";
 import Map from "./composants/Map";
-import "./style.css"
+import "./style.css";
 
 export default function App() {
+  const [type, setType] = useState("sante");
+
+  const locations = [
+    {
+      lat: 45.50493439044056,
+      lng: -73.61313521788279,
+    },
+    {
+      lat: 45.50442924805278,
+      lng: -73.60058255034939,
+    },
+    {
+      lat: 45.55442924805278,
+      lng: -73.63058255034939,
+    },
+    {
+      lat: 45.52442924805278,
+      lng: -73.62058255034939,
+    },
+  ];
+
   return (
     <div className="h-screen">
-      <Map />
-      <LeftMenu />
+      <Map type={type} locations={locations} centre={locations[0]} />
+      <LeftMenu changer={setType} />
     </div>
   );
 }
