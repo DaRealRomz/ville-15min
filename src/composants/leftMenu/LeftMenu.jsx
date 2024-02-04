67303f4d37663f25538e9cade03c94fbaef71a6d
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2024-02-03 19:19:39 -0500

    fix d'erreurs

--- src/composants/leftMenu/LeftMenu.jsx
+++ src/composants/leftMenu/LeftMenu.jsx
@@ -1,37 +1,33 @@
-import { useState } from 'react';
-import './leftMenu.css';
+import { useState } from "react";
+import "./leftMenu.css";
 import typesLieux from "../TypesLieux.json";
 
-export default function LeftMenu({changerType}) {
+export default function LeftMenu({ changerType }) {
+  const [retracted, setRetracted] = useState(false);
 
-    let [retracted, setRetracted] = useState(false);
-
-
-
-    return (<>
-        <div id='left-menu-container' style={{ transform: retracted ? 'translateX(calc(-100% + 40px))' : '' }}>
-            <div id='left-menu-interior' >
-                <input id="input-recherche" type="search" placeholder="  Rechercher un lieu" />
-                <div id="div-boutons">
-                    <div id="div-boutons">
-                        <>
-                            {
-                                typesLieux.map((lieu) => <button class="bouton-service" type="button" onClick={()=>changerType(lieu.type)}><img src={lieu.icon} />{lieu.nom}</button>)
-                            }
-                        </>
-
-                    </div>
-                    
-                </div>
-                <div id="transport-commun">
-                    <input type="checkbox" class="checkbox2" />
-                    <label for="vehicle1"> Présence de vélo</label><br></br>
-                    <input type="checkbox" class="checkbox2" />
-                    <label for="vehicle1"> Présence de transport en commun</label>
-                </div>
-            </div>
-            <button id="menu-toggle" type="button" onClick={() => setRetracted(isRetracted => !isRetracted)}></button>
+  return (
+    <div id="left-menu-container" style={{ transform: retracted ? "translateX(calc(-100% + 40px))" : "" }}>
+      <div id="left-menu-interior">
+        <input id="input-recherche" type="search" placeholder="  Rechercher un lieu" />
+        <div id="div-boutons">
+          <div id="div-boutons">
+            {typesLieux.map((lieu) => (
+              <button key={lieu.type} className="bouton-service" type="button" onClick={() => changerType(lieu.type)}>
+                <img src={lieu.icon} alt="" />
+                {lieu.nom}
+              </button>
+            ))}
+          </div>
         </div>
-    </>);
-
-}   
+        <div id="transport-commun">
+          <input id="velo" type="checkbox" className="checkbox2" />
+          <label htmlFor="velo"> Présence de vélo</label>
+          <br />
+          <input id="transport" type="checkbox" className="checkbox2" />
+          <label htmlFor="transport"> Présence de transport en commun</label>
+        </div>
+      </div>
+      <button id="menu-toggle" type="button" onClick={() => setRetracted((isRetracted) => !isRetracted)}>{retracted ? ">" : "x"}</button>
+    </div>
+  );
+}
