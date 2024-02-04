bf0de5f640ddc310a42b64c0e0ed8a5a38ce8d77
Jean-Félix Morency <jean-felix.morency@polymtl.ca>
2024-02-03 19:35:33 -0500

    Utiliser les emplacements de Google Maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from "react";
 import LeftMenu from "./composants/leftMenu/LeftMenu";
 import Map from "./composants/Map";
-import "./style.css";
+
+const poly = {
+  lat: 45.5047939,
+  lng: -73.615758
+}
 
 export default function App() {
   const [type, setType] = useState("sante");
 
-  const locations = [
-    {
-      lat: 45.50493439044056,
-      lng: -73.61313521788279,
-    },
-    {
-      lat: 45.50442924805278,
-      lng: -73.60058255034939,
-    },
-    {
-      lat: 45.55442924805278,
-      lng: -73.63058255034939,
-    },
-    {
-      lat: 45.52442924805278,
-      lng: -73.6205825503493,
-    },
-  ];
-
   return (
     <div className="h-screen">
-      <Map type={type} locations={locations} centre={locations[0]} />
+      <Map type={type} centre={poly} />
       <LeftMenu changerType={setType} />
     </div>
   );
--- src/composants/Map.jsx
+++ src/composants/Map.jsx
@@ -1,6 +1,9 @@
-import { GoogleMap, useJsApiLoader, Circle, Marker } from "@react-google-maps/api";
-import { useState } from "react";
+import { Circle, GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { Fragment, useEffect, useState } from "react";
 import typesLieux from "./TypesLieux.json";
+
+const DEBOUNCE_DELAY = 500;
+const libraries = ["places"];
 
 const containerStyle = {
   width: "100%",
@@ -20,15 +23,36 @@ const mapStyles = [
   ],
 ];
 
-export default function Map({ type, centre, locations }) {
+export default function Map({ type, centre }) {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.MAPS_API_KEY,
+    libraries,
   });
 
-  const [map, setMap] = useState();
+  const [locations, setLocations] = useState([]);
+  const [map, setMap] = useState(null);
+  const [bounds, setBounds] = useState(null);
 
-  const lieu = typesLieux.find(l => l.type === type);
+  const lieu = typesLieux.find((l) => l.type === type);
+
+  useEffect(() => {
+    if (!map || !bounds) return () => {};
+    const timeout = setTimeout(() => {
+      const service = new window.google.maps.places.PlacesService(map);
+      service.nearbySearch({ bounds, type: lieu.filter }, (results, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK)
+          setLocations(
+            results.map((result) => ({
+              id: result.reference,
+              name: result.name,
+              location: result.geometry.location,
+            })),
+          );
+      });
+    }, DEBOUNCE_DELAY);
+    return () => clearTimeout(timeout);
+  }, [map, bounds, lieu]);
 
   const optionsCercle = { strokeColor: lieu.contour, strokeOpacity: 0.8, fillOpacity: 0.4, fillColor: lieu.fill };
 
@@ -40,20 +64,21 @@ export default function Map({ type, centre, locations }) {
       zoom={12}
       stylers={mapStyles}
       onLoad={(m) => setMap(m)}
-      onUnmount={() => setMap()}
+      onUnmount={() => setMap(null)}
+      onBoundsChanged={() => setBounds(map?.getBounds())}
     >
-      {locations.map((position) => (
-        <>
-          <Circle options={optionsCercle} center={position} radius={1500} map={map} />
+      {locations.map((location) => (
+        <Fragment key={location.id}>
+          <Circle options={optionsCercle} center={location.location} radius={1500} map={map} />
           <Marker
-            position={position}
+            position={location.location}
             map={map}
-            options={{ title: "Poly!" }}
+            options={{ title: location.name }}
             icon={{
               url: lieu.icon,
             }}
           />
-        </>
+        </Fragment>
       ))}
     </GoogleMap>
   ) : (
