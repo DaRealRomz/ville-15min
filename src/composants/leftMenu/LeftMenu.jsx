0d1ac3849187d0012a0a3964067b528aed97a932
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2024-02-03 20:20:27 -0500

    chengement de rayon

--- src/composants/leftMenu/LeftMenu.jsx
+++ src/composants/leftMenu/LeftMenu.jsx
@@ -5,6 +5,21 @@ import typesLieux from "../TypesLieux.json";
 export default function LeftMenu({ changerType, changerRayon }) {
   const [retracted, setRetracted] = useState(false);
 
+  let rayonMarche = 1500;
+
+  function setRayon() {
+    const veloChkbx = document.getElementById('velo');
+    const transportChkbx = document.getElementById('transport');
+
+    if (transportChkbx.checked) {
+      changerRayon(4500);
+    } else if (veloChkbx) {
+      changerRayon(3500);
+    } else {
+      changerRayon(rayonMarche);
+    }
+  }
+
   return (
     <div id="left-menu-container" style={{ transform: retracted ? "translateX(calc(-100% + 40px))" : "" }}>
       <div id="left-menu-interior">
@@ -20,10 +35,10 @@ export default function LeftMenu({ changerType, changerRayon }) {
           </div>
         </div>
         <div id="transport-commun">
-          <input id="velo" type="checkbox" className="checkbox2" />
+          <input id="velo" type="checkbox" className="checkbox2" onClick={setRayon} />
           <label htmlFor="velo"> Présence de vélo</label>
           <br />
-          <input id="transport" type="checkbox" className="checkbox2" />
+          <input id="transport" type="checkbox" className="checkbox2" onClick={setRayon} />
           <label htmlFor="transport"> Présence de transport en commun</label>
         </div>
       </div>
